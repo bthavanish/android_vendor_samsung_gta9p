8e22c9b8721c8bc55063ce445b49dddfcf86c213
bthavanish <bthavanish@users.noreply.github.com>
2026-07-07 02:31:55 +0530

    fix: add missing proprietary.mk for device-specific blobs
    Signed-off-by: bthavanish <bthavanish@gmail.com>

---
+++ proprietary.mk
@@ -0,0 +1,5 @@
+# Device-specific proprietary blobs
+
+PRODUCT_COPY_FILES += \
+    vendor/samsung/gta9p/proprietary/vendor/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini \
+    vendor/samsung/gta9p/proprietary/vendor/etc/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
